81b6bfa8eccea17a6c9c2adba95e0332e3eb1f6c
Márton Spiteller <spitellermarton@gmail.com>
2025-12-02 18:06:05 +0100

    add product section, add addToCart func

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+import { useDispatch } from 'react-redux';
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({});
+
+    const dispatch = useDispatch();
+
+    const handleAddToCart = (item) => {
+        dispatch(addItem(item));
+        setAddedToCart((prevState) => ({
+            ...prevState,
+            [item.name]: true
+        }));
+    }
 
     const plantsArray = [
         {
@@ -269,12 +283,34 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" stroke_inejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
             {!showCart ? (
                 <div className="product-grid">
+                    {
+                        plantsArray.map((item, index) => {
+                            return <div key={index}>
+                                <h2>{item.category}</h2>
+                                <div className="product-list">
+                                    {
+                                        item.plants.map((plant, i) => (
+                                            <div className="product-card" key={`${i}${plant.name}`}>
+                                                <img className="product-image" src={plant.image} alt={plant.name} width={300} />
+                                                <h3 className="product-title">{plant.name}</h3>
+                                                <p>{plant.description}</p>
+                                                <p className="product-price">{plant.cost}</p>
+                                                <button className="product-button"
+                                                    onClick={() => handleAddToCart(plant)}
+                                                >Add to Cart</button>
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                            </div>
 
+                        })
+                    }
 
                 </div>
             ) : (
